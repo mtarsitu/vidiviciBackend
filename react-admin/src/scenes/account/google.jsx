8161fbeee90575aad0ee9cee0e1526fa49734164
mtarsitu <mario.tarsitu@gmail.com>
2023-02-17 18:06:30 +0200

    cleaning up google

--- react-admin/src/scenes/account/google.jsx
+++ react-admin/src/scenes/account/google.jsx
@@ -37,7 +37,7 @@ const Google = () => {
   };
   const handleGoogleSignIn = async () => {
     try {
-      let result = await googleSignIn();
+      await googleSignIn();
     } catch (error) {
       console.log(error);
     }
